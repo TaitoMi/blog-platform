62b39fc6cb0a186f66c62d2376abc2ea6d445553
TaitoMi <gfdaker96@gmail.com>
2020-01-09 16:25:34 +0300

    delete likes

--- src/actions/like.js
+++ src/actions/like.js
@@ -1,7 +1,25 @@
 const likeOrDislike = (isFavorited, slug, token) => async dispatch => {
-  // if (!isFavorited) {
+  if (!isFavorited) {
+    const response = await fetch(
+      `https://conduit.productionready.io/api/articles/${slug}/favorite`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`,
+        },
+      }
+    );
+    const action = await response.json().then(el => {
+      return {
+        type: 'LIKE',
+        payload: el,
+      };
+    });
+    return dispatch(action);
+  }
   const response = await fetch(`https://conduit.productionready.io/api/articles/${slug}/favorite`, {
-    method: 'POST',
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Token ${token}`,
@@ -14,8 +32,6 @@ const likeOrDislike = (isFavorited, slug, token) => async dispatch => {
     };
   });
   return dispatch(action);
-  // }
-  // return dispatch({ type: 'DISLIKE' });
 };
 
 export default likeOrDislike;
